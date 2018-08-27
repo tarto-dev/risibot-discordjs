0837936427e63fee7183569c8e87aa8afbd5e052
Benftwc <benftwc@gmail.com>
2018-08-28 00:04:27 +0200

    Async World <3

--- guestabot.js
+++ guestabot.js
@@ -23,9 +23,11 @@ client.on("guildDelete", guild => {
     client.user.setActivity(`${prefix} - Propage la bonne parole sur ${client.guilds.size} serveurs`);
 });
 
-client.on('message', msg => {
+client.on('message',async msg => {
     // Prevent "botception".
     if (msg.author.bot) return;
+
+    if(message.content.indexOf(config.prefix) !== 0) return;
 
     if (config.debug && msg.author.id != config.root_user) {
         return;
@@ -62,9 +64,8 @@ client.on('message', msg => {
 
         if (command.startsWith('LEGANGE')) {
 
-            // So we get our messages, and delete them. Simple enough, right?
-            const fetched = msg.channel.fetchMessages({limit: 500});
-            msg.channel.bulkDelete(fetched)
+            const fetched = await message.channel.fetchMessages({limit: 500});
+            message.channel.bulkDelete(fetched)
                 .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
 
             msg.reply(":ok_hand: :grin:").delete(500);
