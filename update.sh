9bf990165b5f564ec526fe3bc9ebea16e0d5aeca
Benftwc <benftwc@gmail.com>
2018-08-27 23:49:54 +0200

    no message

--- update.sh
+++ update.sh
@@ -2,8 +2,14 @@
 
 pm2 stop guestabot
 
-git reset guestabot.js --hard
+git checkout guestabot.js
+git checkout config.json
+git checkout package.json
+
 git pull
+
+npm i
+
 export LASTPULLID=$(git log --format="%H" -n 1) | awk '{print substr($0,0,5)}'
 
 sed -i "1 i\var lastID=\"$LASTPULLID\";" guestabot.js
