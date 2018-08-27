3867d73498765583ebe88a3c361ecccd4cbde735
Benftwc <benftwc@gmail.com>
2018-08-27 23:32:27 +0200

    Core upgrade :-)

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@
 
 git pull
 export LASTPULLID=$(git log --format="%H" -n 1)
-sed -i '1 i\var lastID="$LASTPULLID";' guestabot.js
+
+sed -i "1 i\var lastID=\"$LASTPULLID\";" guestabot.js
 pm2 restart guestabot
