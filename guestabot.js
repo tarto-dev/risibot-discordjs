266c6b8e4c750c653de867ea7e873efe8692ad35
Benftwc <benftwc@gmail.com>
2018-08-28 10:19:17 +0200

    no message

--- guestabot.js
+++ guestabot.js
@@ -36,9 +36,10 @@ client.on("guildDelete", guild => {
 client.on('message', async msg => {
     // Prevent "botception".
     if (msg.author.bot) return;
-
-    if(msg.content.indexOf("bot") !== 0) {
-        msg.reply("Hmm ? Ça parle de moi ?");
+    var troll_bot_idx = msg.content.indexOf("bot");
+    var troll_answers = ["Hmm ? Ça parle de moi ?", "T'as un soucis a parler de moi ? Tu veux que j'appelle Shlomo ?", "Trouve toi un pote frère, arrête de me citer comme ça..."];
+    if(troll_bot_idx > 0) {
+        msg.reply(troll_answers[Math.floor(Math.random()*troll_answers.length)]);
     }
 
     if (msg.content.indexOf(config.prefix) !== 0) return;
