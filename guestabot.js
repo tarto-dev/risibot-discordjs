e0eb90a8647523656f648f191fd2720fbd9f6a8a
Benftwc <benftwc@gmail.com>
2018-08-29 00:49:27 +0200

    no message

--- guestabot.js
+++ guestabot.js
@@ -81,7 +81,7 @@ client.on('message', async msg => {
 
         if(command.startsWith("credits")) {
             msg.channel.send("Merci à la risibank (https://risibank.fr/) et à l'élite (https://2sucres.org/)")
-            msg.channel.send("Dev par @BISOU MAGIQUE EN SPRAY#9153 avec les encouragements de @Ourx#4698");
+            msg.channel.send("Dev par poneygenial avec les encouragements de Ourx");
         }
 
         if(command.startsWith('RISITAGS') && no_access(msg)) {
