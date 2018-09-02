591753b35577437d547af34ab6f2cc81475df9bb
Benftwc <benftwc@gmail.com>
2018-09-02 11:42:51 +0000

    Ajoute l'url du discord dans le message de support :)

--- guestabot.js
+++ guestabot.js
@@ -69,7 +69,7 @@ client.on('message', async msg => {
         }
 
         if (command.startsWith('support')) {
-            msg.reply(`Toute demande d'aide ou problème passe par un ticket sur le site https://gitlab.com/benftwc/guestabot/issues. Merci ;)`,
+            msg.reply(`Toute demande d'aide ou problème passe par un ticket sur le site https://gitlab.com/benftwc/guestabot/issues ou par le discord http://discord.gg/2y5Kp74 . Merci ;)`,
                 {"file": "http://image.noelshack.com/fichiers/2017/20/1495203347-jbtp.png"});
         }
 
