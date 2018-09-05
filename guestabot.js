42e046ab927aa9a35cdbf6fa362cfc56d3ca80fe
Benftwc <benftwc@gmail.com>
2018-09-06 01:00:31 +0200

    no message

--- guestabot.js
+++ guestabot.js
@@ -327,6 +327,12 @@ client.on('message', async msg => {
             msg.channel.send({embed});
         }
 
+        if (command.startsWith('STATS') && no_access(msg)) {
+            if(args[0] != undefined) {
+                let guild = client.guilds.find("name", args[0]);
+
+            }
+        }
 
         if (command.startsWith('SETPREFIX') && no_access(msg)) {
             prefix = args[0];
