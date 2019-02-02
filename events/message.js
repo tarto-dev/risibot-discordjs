442b3dc0c81b008530d1d07b021c2dd46d08ac94
CASSINAT Benjamin <benftwc@gmail.com>
2019-02-02 19:21:12 +0100

    [Update scoring]

--- events/message.js
+++ events/message.js
@@ -4,27 +4,62 @@ module.exports = (client, message) => {
 
     if (!message.guild) return;
 
+    const computeScore = (score) => {
+        console.log("IN");
+        console.log(score);
+        let levels = [], num = 1;
+        levels[0] = {"level": 0, "xp": 0, "diff": 0};
+        while (num < 200) {
+
+            let xp = Math.round(5 * ((num ** 3) / 2) + 50 * num + 100);
+            let diff = xp - (levels[num-1].xp);
+
+            let calcul = {
+                "level" : num,
+                "xp": xp,
+                "diff": diff
+            };
+
+            levels.push(calcul);
+            num++;
+
+            if(score < xp) {
+                break;
+            }
+
+        }
+
+        const xpRange = {"min": 10, "max": 150};
+        let currentXp = score + (Math.floor(Math.random() * (xpRange.max - xpRange.min + 1)) + xpRange.min);
+
+        let scoring = {xp: currentXp, level: levels[levels.length-1].level};
+        console.log("OUT");
+        console.log(scoring);
+        return scoring;
+    }
+
+
     const guildConf = client.settings.ensure(message.guild.id, client.defaultSettings);
     client.guildConf = guildConf;
 
     ///////////////////////////////////////////////////
     //////////////////      INIT        ///////////////
     ///////////////////////////////////////////////////
-    if(!client.guildConf.hasOwnProperty('score') || client.guildConf.score === undefined) {
+    if (!client.guildConf.hasOwnProperty('score') || client.guildConf.score === undefined) {
         client.guildConf.score = true;
     }
 
-    if(!client.guildConf.hasOwnProperty('nsfwOnly') || client.guildConf.nsfwOnly === undefined) {
+    if (!client.guildConf.hasOwnProperty('nsfwOnly') || client.guildConf.nsfwOnly === undefined) {
         client.guildConf.nsfwOnly = "off";
     }
-    
-    if(!client.guildConf.hasOwnProperty('history') || client.guildConf.history === undefined) {
+
+    if (!client.guildConf.hasOwnProperty('history') || client.guildConf.history === undefined) {
         client.guildConf.history = 4;
     }
-    if(!client.guildConf.hasOwnProperty('sticker404') || client.guildConf.sticker404 === undefined) {
+    if (!client.guildConf.hasOwnProperty('sticker404') || client.guildConf.sticker404 === undefined) {
         client.guildConf.sticker404 = "http://image.noelshack.com/fichiers/2018/01/5/1515108350-410.png";
     }
-    if(!client.guildConf.hasOwnProperty('disable_vote') || client.guildConf.disable_vote === undefined) {
+    if (!client.guildConf.hasOwnProperty('disable_vote') || client.guildConf.disable_vote === undefined) {
         client.guildConf.disable_vote = false;
     }
 
@@ -41,25 +76,39 @@ module.exports = (client, message) => {
         const key = `${message.guild.id}-${message.author.id}`;
 
         // Triggers on new users we haven't seen before.
-        client.points.ensure(`${message.guild.id}-${message.author.id}`, {
+        client.points.ensure(`${key}`, {
             user: message.author.id,
             guild: message.guild.id,
             points: 0,
-            level: 1
+            level: 1,
+            xp: 0
         });
         client.points.inc(key, "points");
 
-        client.risistory.ensure(message.guild.id, {tags: []});
-        client.risistory.push(message.guild.id, args.join(' '), 'tags', true);
+        if(client.points.get(`${key}`, 'xp') === undefined) {
+            client.points.set(`${key}`, 0, 'xp')
+        }
+
+        if(client.points.get(`${key}`, 'level') === undefined) {
+            client.points.set(`${key}`, 0, 'level')
+        }
+
+        let newScoring = computeScore(client.points.get(`${key}`, 'xp'));
 
         // Calculate the user's current level
-        const curLevel = Math.floor(0.1 * Math.sqrt(client.points.get(key, "points")));
+        const curLevel = newScoring.level;
 
         // Act upon level up by sending a message and updating the user's level in enmap.
-        if (client.points.get(key, "level") < curLevel) {
-            message.reply(`A force de stickers, tu passe au niveau **${curLevel}**! `);
-            client.points.set(key, curLevel, "level");
+        if (curLevel > client.points.get(`${key}`, "level")) {
+            message.reply(`A force de spam des stickers, t'es passé au niveau **${curLevel}**! `);
+            client.points.set(`${key}`, curLevel, "level");
         }
+
+        client.points.set(`${key}`, newScoring.xp, 'xp');
+        client.points.set(`${key}`, newScoring.level, 'level');
+
+        client.risistory.ensure(message.guild.id, {tags: []});
+        client.risistory.push(message.guild.id, args.join(' '), 'tags', true);
     }
 
     // Grab the command data from the client.commands Enmap
--- modules/utils.js
+++ modules/utils.js
@@ -8,7 +8,3 @@ module.exports.has_perm = (message, permission, warn = false) => {
 module.exports.getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-module.exports.removeCaller = (message) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
