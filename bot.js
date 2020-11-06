15ec3281892c6c8390d4a3fadbb3e0f9b57ac298
Benjamin <benftwc@users.noreply.github.com>
2020-11-06 15:43:57 +0100

    fix

--- bot.js
+++ bot.js
@@ -2,14 +2,16 @@ const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
 
-import * as Sentry from "@sentry/node";
-import * as Tracing from "@sentry/tracing";
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 
 Sentry.init({
-    dsn: "https://64e48c5959c74d13b1abf2d43baffb4b@o472921.ingest.sentry.io/5507216" });
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0,
+    dsn: "https://64e48c5959c74d13b1abf2d43baffb4b@o472921.ingest.sentry.io/5507216"
+});
+
+// We recommend adjusting this value in production, or using tracesSampler
+// for finer control
+tracesSampleRate: 1.0,
 });
 
 const client = new Discord.Client();
@@ -36,11 +38,13 @@ const defaultSettings = {
     sticker404: "http://image.noelshack.com/fichiers/2018/01/5/1515108350-410.png"
 };
 
-client.risistory = new Enmap({name: "risistory",
+client.risistory = new Enmap({
+    name: "risistory",
     fetchAll: false,
     autoFetch: true,
-    cloneLevel: 'deep'});
-client.points = new Enmap({name: "points"});
+    cloneLevel: 'deep'
+});
+client.points = new Enmap({ name: "points" });
 client.defaultSettings = defaultSettings;
 
 const risicount = require('./risicount.json');
